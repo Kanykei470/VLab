5091574896740dce47d6ae63314569c1b09eb287
Kanykei470 <akanykej73@gmail.com>
2023-06-06 18:27:13 +0600

    Добавление окна меню

--- obj/Debug/net6.0-windows/App.g.i.cs
+++ obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace VLab {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/VLab;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VLab;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net6.0-windows/Views/ResoursesWindow.g.i.cs
+++ obj/Debug/net6.0-windows/Views/ResoursesWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace VLab.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VLab;component/views/resourseswindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VLab;V1.0.0.0;component/views/resourseswindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ResoursesWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
