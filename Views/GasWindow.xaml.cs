b09834830cb09df0b56d30a5a8c3b0e766770bf4
Kanykei470 <akanykej73@gmail.com>
2023-06-11 19:37:44 +0600

    Добавить таблицу

--- Views/GasWindow.xaml.cs
+++ Views/GasWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace VLab.Views
         private SolidColorBrush brush;
         private ColorAnimation colorAnimation;
 
+       
         public GasWindow()
         {
             InitializeComponent();
@@ -61,6 +62,23 @@ namespace VLab.Views
                 connection.Close();
             }
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT Name FROM Gasses";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string groupName = reader.GetString(0);
+                    comboBox.Items.Add(groupName);
+                }
+
+                reader.Close();
+            }
+
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -75,29 +93,92 @@ namespace VLab.Views
                 // Если таймер уже существует, останавливаем его
                 timer.Stop();
             }
+
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Timer_Tick;
             timer.Start();
 
-
-
-
             Filter.Background = Brushes.White;
             Filter.Name = "Filter"; // Устанавливаем идентификатор элемента
-            ApplyAnimationToCanvas(Filter);
+
+
+
+            string selectedValue = comboBox.SelectedItem.ToString(); // Получаем выбранное значение из комбобокса
+
+            string query = "SELECT id_Gases FROM Gasses WHERE Name = @name"; // Замените YourTable на имя вашей таблицы
+            int id;
+
+           
+            using (SqlConnection connection = new SqlConnection(connectionString)) // Замените connectionString на вашу строку подключения
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@name", selectedValue);
+
+                    object result = command.ExecuteScalar();
+                    if (result != null && int.TryParse(result.ToString(), out id))
+                    {
+                        // Шаг 2: Использование ID для извлечения названия из другой таблицы
+
+                        string nameQuery = "SELECT Name FROM Gas_Colors WHERE id_Gas_Colors = @id"; // Замените AnotherTable на имя вашей другой таблицы
+                        string name;
+
+                        using (SqlCommand nameCommand = new SqlCommand(nameQuery, connection))
+                        {
+                            nameCommand.Parameters.AddWithValue("@id", id);
+
+                            object nameResult = nameCommand.ExecuteScalar();
+                            if (nameResult != null)
+                            {
+                                name = nameResult.ToString();
+                                ApplyAnimationToCanvas(Filter, name);
+                                ColorBefore.Content = "white";
+                                ColorAfter.Content = name;
+                                PDK.Content = CalculatePDK(id);
+                            }
+                        }
+                    }
+                }
+            }
+
 
         }
+
+        public int CalculatePDK(int id)
+        {
+            switch (id)
+            {
+                case 1: return 200;
+                case 2: return 20;
+                case 3: return 20;
+                case 4: return 100;
+                case 5: return 20;
+                case 6: return 20;
+                case 7: return 100;
+                case 8: return 20;
+                case 9: return 5;
+                case 10: return 10;
+                case 11: return 50;
+                case 12: return 300;
+                case 13: return 1;
+                case 14: return 1000;
+                default:return 0;
+            }
+
+        }
+
 
         private void Button_Stop(object sender, RoutedEventArgs e)
         {
             timer.Stop();
-            //Weight.Content = weight.ToString();
+            Random random = new Random();
+            Air.Content = random.Next(200, 400).ToString();
+            C.Content = random.Next(10, 1000).ToString();
 
-            //Random random = new Random();
-            //Pressure.Content = random.Next(748, 760).ToString();
 
-            //Air_Through_Rotameter.Content = random.Next(10, 1000).ToString();
-            //Volume_Of_Air_Reduced_To_Standard.Content= random.Next(10, 1000).ToString();
+
 
             if (brush != null && colorAnimation != null)
             {
@@ -110,7 +191,7 @@ namespace VLab.Views
                 brush = new SolidColorBrush(currentColor);
                 Filter.Background = brush;
             }
-
+           
 
         }
 
@@ -119,15 +200,22 @@ namespace VLab.Views
            
             count++;
             Time.Content = count.ToString();
-            //weight = (count * 0.014);
+            
+           
+
         }
 
 
-        private void ApplyAnimationToCanvas(System.Windows.Controls.Canvas canvas)
+        private void ApplyAnimationToCanvas(System.Windows.Controls.Canvas canvas,string colorName)
         {
             colorAnimation = new ColorAnimation();
             colorAnimation.From = Colors.White;
-            colorAnimation.To = Colors.Red;
+            string colors =  colorName;
+            Color color = (Color)ColorConverter.ConvertFromString(colors); // Преобразуем строку в объект Color
+
+
+
+            colorAnimation.To = color;
             colorAnimation.Duration = TimeSpan.FromSeconds(30);
 
             brush = new SolidColorBrush(Colors.White);
